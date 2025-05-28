e662c6a73a7eea367764a98f7cb2c1c8bd59ec28
isabela Barcellos Freire <isabelabarcellos0812@gmail.com>
2025-05-28 13:27:09 -0300

    iniciando projeto

---
+++ VIAF/src/test/java/br/com/fiap/VIAF/ViafApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.VIAF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
